c360e957186e9ce09c46b8387e3fb65a3f56ba99
azamovbehruz <azamovbehruz@gmail.com>
2024-01-23 20:00:41 +0500

    Full ready

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/products">Products</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
